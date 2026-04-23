36d387fd15260c47d64962479ab7f5f75d080df3
iovitz <iovitz1999@gmail.com>
2026-04-23 11:58:56 +0800

    feat: 添加wezterm配置

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,107 @@
+-- discord.gg/boronide, code generated using luamin.js™
+-- 加载 wezterm API 和获取 config 对象
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+-- 检测 macOS
+local is_macos = wezterm.target_triple:find("darwin") ~= nil
+
+-- 根据操作系统选择修饰键
+local function ctrl_or_super()
+    return is_macos and "SUPER" or "CTRL"
+end
+
+-------------------- 颜色配置 --------------------
+
+-- config.color_scheme = 'Aurora'
+
+config.color_scheme = 'Catppuccin Mocha'
+config.window_decorations = "TITLE | RESIZE"
+config.use_fancy_tab_bar = false
+config.enable_tab_bar = true
+config.show_tab_index_in_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = false
+
+config.inactive_pane_hsb = {
+    saturation = 0.9,
+    brightness = 0.8
+}
+
+-- 设置字体和窗口大小
+config.font = wezterm.font_with_fallback {"JetBrainsMono Nerd Font", "Noto Sans CJK SC"}
+config.font_size = 12
+config.initial_cols = 80
+config.initial_rows = 20
+
+-- 设置默认的启动shell
+-- config.default_prog = {'C:\\Windows\\System32\\wsl.exe'}
+
+-------------------- 键盘绑定 --------------------
+
+-- 关闭默认键位
+config.disable_default_key_bindings = true
+
+local act = wezterm.action
+
+config.leader = {
+    key = ';',
+    mods = ctrl_or_super(),
+    timeout_milliseconds = 1000
+}
+
+config.keys = {{
+    key = 'c',
+    mods = ctrl_or_super() .. '|SHIFT',
+    action = act.CopyTo("Clipboard")
+}, {
+    key = 'r',
+    mods = 'SUPER',
+    action = act.SplitVertical {
+        domain = 'CurrentPaneDomain'
+    }
+}, {
+    key = 'v',
+    mods = ctrl_or_super() .. '|SHIFT',
+    action = act.PasteFrom("Clipboard")
+}, {
+    key = 'w',
+    mods = 'CTRL',
+    action = act.CloseCurrentTab {
+        confirm = false
+    }
+}}
+
+for i = 1, 8 do
+    -- CTRL + number to activate that tab
+    table.insert(config.keys, {
+        key = tostring(i),
+        mods = ctrl_or_super(),
+        action = act.ActivateTab(i - 1)
+    })
+
+end
+
+-------------------- 鼠标绑定 --------------------
+
+config.mouse_bindings = {{
+    event = {
+        Up = {
+            streak = 1,
+            button = 'Left'
+        }
+    },
+    mods = 'NONE',
+    action = act.CompleteSelection 'ClipboardAndPrimarySelection'
+}, -- Open HyperLink
+{
+    event = {
+        Up = {
+            streak = 1,
+            button = 'Left'
+        }
+    },
+    mods = ctrl_or_super(),
+    action = act.OpenLinkAtMouseCursor
+}}
+
+return config
