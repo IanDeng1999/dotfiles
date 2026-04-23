df0ba48a87e198acf7cb1834725478a86d0ee02d
iovitz <iovitz1999@gmail.com>
2026-04-23 12:51:28 +0800

    feat: 添加zim

--- initial.sh
+++ initial.sh
@@ -1,4 +1,9 @@
 ln -s "$(pwd)/nvim" ~/.config/nvim 
 ln -s "$(pwd)/wezterm" ~/.config/wezterm
 ln -s "$(pwd)/zellij" ~/.config/zellij
-ln -s "$(pwd)/pi" ~/.pi
+
+ln -s "$(pwd)/.pi" ~/.pi
+
+ln -s "$(pwd)/zsh/.zshrc" ~/.zshrc
+ln -s "$(pwd)/zsh/.zimrc" ~/.zimrc
+ln -s "$(pwd)/zsh/.zim" ~/.zim
