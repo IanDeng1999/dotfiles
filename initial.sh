eed45d035376099c60a2bbf08789eedf56c348a7
iovitz <iovitz1999@gmail.com>
2026-04-24 14:24:48 +0800

    feat: 优化配置脚本

--- initial.sh
+++ initial.sh
@@ -1,11 +1,11 @@
-ln -sf "$(pwd)/nvim" ~/.config/nvim 
-ln -sf "$(pwd)/wezterm" ~/.config/wezterm
-ln -sf "$(pwd)/zellij" ~/.config/zellij
+ln -sf "$(pwd)/nvim" ~/.config
+ln -sf "$(pwd)/wezterm" ~/.config
+ln -sf "$(pwd)/zellij" ~/.config
 
-ln -sf "$(pwd)/pi/prompts" ~/.pi/agent/prompts
-ln -sf "$(pwd)/pi/skills" ~/.pi/agent/skills
-ln -sf "$(pwd)/pi/SYSTEM.md" ~/.pi/agent/SYSTEM.md
+ln -sf "$(pwd)/.agents" ~
+ln -sf "$(pwd)/.pi/prompts" ~/.pi/agent
+ln -sf "$(pwd)/.pi/SYSTEM.md" ~/.pi/agent
 
-ln -sf "$(pwd)/zsh/.zshrc" ~/.zshrc
-ln -sf "$(pwd)/zsh/.zimrc" ~/.zimrc
-ln -sf "$(pwd)/zsh/.zim" ~/.zim
+ln -sf "$(pwd)/zsh/.zshrc" ~
+ln -sf "$(pwd)/zsh/.zimrc" ~
+ln -sf "$(pwd)/zsh/.zim" ~
