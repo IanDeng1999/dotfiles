ff7ea1ccbb03d3fc62d18691da04a7110beb0f98
IanDeng1999 <ian.deng.x@gmail.com>
2026-08-03 19:06:41 +0800

    Remove leftover dotbot files

--- run_once_before_install-zshrc-source.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Add source line to ~/.zshrc if not already present
-ZSHRC="$HOME/.zshrc"
-SOURCE_LINE='[[ -f ~/.myzshrc ]] && source ~/.myzshrc'
-
-if ! grep -qF "$SOURCE_LINE" "$ZSHRC" 2>/dev/null; then
-  echo "" >> "$ZSHRC"
-  echo "$SOURCE_LINE" >> "$ZSHRC"
-fi
